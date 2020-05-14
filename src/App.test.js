4a46ce69e752c871c953e2c83219764da51badc2
derek-cornelison <dcornelison1216@gmail.com>
2020-05-14 13:42:37 -0400

    initial render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import * as rtl from 'react-testing-library';
-import 'jest-dom/extend-expect';
+import * as rtl from '@testing-library/react';
+import * as jd from '@testing-library/jest-dom';
 import App from "./App";
 
-test("renders App without crashing", () => {
-  render(<App />);
+
+test("renders fname, lname, email, message fields & button without crashing", () => {
+  const container = rtl.render(<App />);
+
+  const fname = container.queryByText(/first name/i);
+  const lname = container.queryByText(/last name/i);
+  const email = container.queryByText(/email/i);
+  const message = container.queryByText(/message/i);
+  const submit = container.queryByText(/submit/i);
 });
