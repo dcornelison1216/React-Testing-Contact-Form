8614f704ac21f42d7fcf702579adacfd76ff5291
derek-cornelison <dcornelison1216@gmail.com>
2020-05-14 13:30:19 -0400

    setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
 import App from "./App";
 
 test("renders App without crashing", () => {
