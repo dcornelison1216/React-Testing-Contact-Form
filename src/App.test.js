6361e14e990d79629ab7f9f68fe7aed5789d7a4a
derek-cornelison <dcornelison1216@gmail.com>
2020-05-14 17:09:01 -0400

    working on inputting values

--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,31 @@ import * as jd from '@testing-library/jest-dom';
 import App from "./App";
 
 
-test("renders fname, lname, email, message fields & button without crashing", () => {
+test("renders fname, lname, email, message fields without crashing", () => {
   const container = rtl.render(<App />);
 
   const fname = container.queryByText(/first name/i);
+  expect(fname).toBeInTheDocument();
   const lname = container.queryByText(/last name/i);
+  expect(lname).toBeInTheDocument();
   const email = container.queryByText(/email/i);
+  expect(email).toBeInTheDocument();
   const message = container.queryByText(/message/i);
-  const submit = container.queryByText(/submit/i);
+  expect(message).toBeInTheDocument();
+});
+
+// const setup = () => {
+//   const container = rtl.render(<App />)
+//   const input = container.queryByText('first name')
+//   return {
+//     input,
+//     ...container
+//   }
+// }
+
+test("fname error if name > 3 characters", () => {
+  const container = rtl.render(<App />);
+  const fname = container.queryByText('first name');
+  fname.value.change(fname, { target: {value: 'Derek'} });
+  expect(fname.value).toBe('Derek');
 });
